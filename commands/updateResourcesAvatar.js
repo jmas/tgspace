30180379e257913e703904501d968dfeb22bd7fe
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2022-07-20 15:33:41 +0300

    Fix avatar fetch algo

--- commands/updateResourcesAvatar.js
+++ commands/updateResourcesAvatar.js
@@ -1,17 +1,20 @@
-const { runProcesses } = require("../utils/process");
+// const { runProcesses } = require("../utils/process");
 const { upsert } = require("../utils/supabase");
 const { getResourcesForUpdateAvatar } = require("../utils/queries");
+const fetchResourcesAvatar = require("../operations/fetchResourcesAvatar");
 
 const updateResourcesAvatars = async () => {
   const updateLimit = 500; // parseInt(process.env.UPDATE_LIMIT || "1000", 10);
 
   const resources = await getResourcesForUpdateAvatar(updateLimit);
 
-  const result = await runProcesses(
-    "./processes/fetchResourcesAvatar",
-    resources,
-    2
-  );
+  // const result = await runProcesses(
+  //   "./processes/fetchResourcesAvatar",
+  //   resources,
+  //   2
+  // );
+
+  const result = await fetchResourcesAvatar(resources);
 
   await upsert("resource", result);
 
